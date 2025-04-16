466d5b166502bd6bc8dc9fa63b068ab9947b19ad
AribNoor-dev <techlightguru@gmail.com>
2025-04-16 19:54:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import time
 app = Flask(__name__)
 
 url = "https://jeemain.nta.nic.in/"
-keywords = ["result","score","card"]  
+keywords = ["result","score","card","answer"]  
 
 def telegram_msg(message):
     token = '7584740207:AAG47e0hC8ghyuR76vX5wKb78mWh6juG894'
